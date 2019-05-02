508e9a898f78298e947d186ae3220443edb868b6
Dylan Kim <mlach9u@gmail.com>
2019-05-02 21:58:31 +0900

    end #2 Tab to spaces
    Change Tab to 4 spaces

--- src/sqlitemp.h
+++ src/sqlitemp.h
@@ -6,182 +6,184 @@
 template< typename _Elem, typename _Traits >
 std::basic_ostream< _Elem, _Traits >& operator <<(std::basic_ostream< _Elem, _Traits >& _Ostr, const sqliteElement< _Elem >& e)
 {
-	switch (e.type())
-	{
-	case SQLITE_INTEGER:	_Ostr << e.as_int();	break;
-	case SQLITE_FLOAT:		_Ostr << e.as_double();	break;
-	case SQLITE_TEXT:		_Ostr << e.as_string(); break;
-	}
-	return _Ostr;
+    switch (e.type())
+    {
+    case SQLITE_INTEGER:	_Ostr << e.as_int();	break;
+    case SQLITE_FLOAT:		_Ostr << e.as_double();	break;
+    case SQLITE_TEXT:		_Ostr << e.as_string(); break;
+    }
+    return _Ostr;
 }
 
 template< typename _Elem >
 struct sqliteDatabase
 {
-	typedef typename sqliteRowSet< _Elem > _Rowset;
-	typedef typename _Rowset::_ColumnSet _Columnset;
-	typedef typename _Columnset::_Element _Element;
-	typedef typename _Element::_String _String;
+    typedef typename sqliteRowSet< _Elem > _Rowset;
+    typedef typename _Rowset::_ColumnSet _Columnset;
+    typedef typename _Columnset::_Element _Element;
+    typedef typename _Element::_String _String;
 
-	typedef typename sqliteExecutor< _Elem > _Executor;
+    typedef typename sqliteExecutor< _Elem > _Executor;
 
-	typedef typename std::shared_ptr< _Rowset > _Rowset_Ptr;
-
-public:
-	sqliteDatabase()
-		: m_SQLite(0), m_nLastError(SQLITE_OK) {}
-
-	sqliteDatabase(const _String& strFileName, int nFlags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, const char* zVfs = 0)
-		: m_SQLite(0), m_nLastError(SQLITE_OK)
-	{
-		open(strFileName, nFlags, zVfs);
-	}
-
-	virtual ~sqliteDatabase()
-	{ close(); }
+    typedef typename std::shared_ptr< _Rowset > _Rowset_Ptr;
 
 public:
-	bool good() const			{ return (m_nLastError == SQLITE_OK); }
-	bool fail() const			{ return !good(); }
-	void clear_err()			{ m_nLastError = SQLITE_OK; }
-	void set_err(int nError)	{ m_nLastError = nError; }
+    sqliteDatabase()
+        : m_SQLite(0), m_nLastError(SQLITE_OK) {}
 
-	int get_err()
-	{
-		if (m_nLastError)	return m_nLastError;
-		else if (m_SQLite)	return sqlite3_errcode(m_SQLite);
-		return 0;
-	}
+    sqliteDatabase(const _String& strFileName, int nFlags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, const char* zVfs = 0)
+        : m_SQLite(0), m_nLastError(SQLITE_OK)
+    {
+        open(strFileName, nFlags, zVfs);
+    }
 
-	int get_exterr()
-	{
-		if (m_SQLite)
-			return sqlite3_extended_errcode(m_SQLite);
-		return 0;
-	}
-
-	void get_errmsg(_String& strError)
-	{
-		if (m_nLastError)	strError.fromutf8(sqlite3_errstr(m_nLastError));
-		else if (m_SQLite)	strError.fromutf8(sqlite3_errmsg(m_SQLite));
-	}
-
-	void throw_errmsg()
-	{
-		if (m_nLastError)	throw std::exception(string().fromutf8(sqlite3_errstr(m_nLastError)).c_str());
-		else if (m_SQLite)	throw std::exception(string().fromutf8(sqlite3_errmsg(m_SQLite)).c_str());
-	}
+    virtual ~sqliteDatabase()
+    {
+        close();
+    }
 
 public:
-	bool open(const _String& strFileName, int nFlags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, const char* zVfs = 0)
-	{
-		if (m_SQLite)
-			close();
-		m_nLastError = sqlite3_open_v2(strFileName.toutf8().c_str(), &m_SQLite, nFlags, zVfs);
-		return (m_nLastError == SQLITE_OK);
-	}
+    bool good() const { return (m_nLastError == SQLITE_OK); }
+    bool fail() const { return !good(); }
+    void clear_err() { m_nLastError = SQLITE_OK; }
+    void set_err(int nError) { m_nLastError = nError; }
 
-	void close()
-	{
-		if (m_SQLite)
-		{
-			m_nLastError = sqlite3_close_v2(m_SQLite);
-			m_SQLite = 0;
-		}
-	}
+    int get_err()
+    {
+        if (m_nLastError)	return m_nLastError;
+        else if (m_SQLite)	return sqlite3_errcode(m_SQLite);
+        return 0;
+    }
+
+    int get_exterr()
+    {
+        if (m_SQLite)
+            return sqlite3_extended_errcode(m_SQLite);
+        return 0;
+    }
+
+    void get_errmsg(_String & strError)
+    {
+        if (m_nLastError)	strError.fromutf8(sqlite3_errstr(m_nLastError));
+        else if (m_SQLite)	strError.fromutf8(sqlite3_errmsg(m_SQLite));
+    }
+
+    void throw_errmsg()
+    {
+        if (m_nLastError)	throw std::exception(string().fromutf8(sqlite3_errstr(m_nLastError)).c_str());
+        else if (m_SQLite)	throw std::exception(string().fromutf8(sqlite3_errmsg(m_SQLite)).c_str());
+    }
+
+public:
+    bool open(const _String & strFileName, int nFlags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, const char* zVfs = 0)
+    {
+        if (m_SQLite)
+            close();
+        m_nLastError = sqlite3_open_v2(strFileName.toutf8().c_str(), &m_SQLite, nFlags, zVfs);
+        return (m_nLastError == SQLITE_OK);
+    }
+
+    void close()
+    {
+        if (m_SQLite)
+        {
+            m_nLastError = sqlite3_close_v2(m_SQLite);
+            m_SQLite = 0;
+        }
+    }
 
 private:
-	bool _execute(const _String& strSql)
-	{
-		_SQLiteStmt SQLiteStmt;
-		m_nLastError = sqlite3_prepare_v3(m_SQLite, strSql.toutf8().c_str(), -1, 0, &SQLiteStmt, 0);
-		if (SQLiteStmt)
-		{
-			_Executor exec(SQLiteStmt);
-			m_nLastError = exec.execute();
-			switch (m_nLastError)
-			{
-			case SQLITE_ERROR:	m_nLastError = get_err();		break;
-			case SQLITE_DONE:	m_nLastError = SQLITE_OK;		break;
-			case SQLITE_ROW:	m_nLastError = SQLITE_MISUSE;	break;
-			}
-		}
-		return (m_nLastError == SQLITE_OK);
-	}
+    bool _execute(const _String & strSql)
+    {
+        _SQLiteStmt SQLiteStmt;
+        m_nLastError = sqlite3_prepare_v3(m_SQLite, strSql.toutf8().c_str(), -1, 0, &SQLiteStmt, 0);
+        if (SQLiteStmt)
+        {
+            _Executor exec(SQLiteStmt);
+            m_nLastError = exec.execute();
+            switch (m_nLastError)
+            {
+            case SQLITE_ERROR:	m_nLastError = get_err();		break;
+            case SQLITE_DONE:	m_nLastError = SQLITE_OK;		break;
+            case SQLITE_ROW:	m_nLastError = SQLITE_MISUSE;	break;
+            }
+        }
+        return (m_nLastError == SQLITE_OK);
+    }
 
-	_Rowset_Ptr _query(const _String& strSql)
-	{
-		_Rowset_Ptr pRet;
-		_SQLiteStmt SQLiteStmt;
-		m_nLastError = sqlite3_prepare_v3(m_SQLite, strSql.toutf8().c_str(), -1, 0, &SQLiteStmt, 0);
-		if (SQLiteStmt)
-		{
-			pRet.reset(new _Rowset(SQLiteStmt));
-			if (!pRet->to_next())
-			{
-				m_nLastError = pRet->get_result();
-				pRet.reset();
-			}
-		}
-		return pRet;
-	}
+    _Rowset_Ptr _query(const _String & strSql)
+    {
+        _Rowset_Ptr pRet;
+        _SQLiteStmt SQLiteStmt;
+        m_nLastError = sqlite3_prepare_v3(m_SQLite, strSql.toutf8().c_str(), -1, 0, &SQLiteStmt, 0);
+        if (SQLiteStmt)
+        {
+            pRet.reset(new _Rowset(SQLiteStmt));
+            if (!pRet->to_next())
+            {
+                m_nLastError = pRet->get_result();
+                pRet.reset();
+            }
+        }
+        return pRet;
+    }
 
 public:
-	template< typename TYPE >
-	bool executeV(const TYPE* lpszFormat, va_list v)
-	{
-		_String strSql;
-		strSql.formatV(lpszFormat, v);
-		return _execute(strSql);
-	}
+    template< typename TYPE >
+    bool executeV(const TYPE * lpszFormat, va_list v)
+    {
+        _String strSql;
+        strSql.formatV(lpszFormat, v);
+        return _execute(strSql);
+    }
 
-	bool execute(const char* lpszFormat, ...)
-	{
-		va_list v;
-		va_start(v, lpszFormat);
-		bool bRet = executeV(lpszFormat, v);
-		va_end(v);
-		return bRet;
-	}
+    bool execute(const char* lpszFormat, ...)
+    {
+        va_list v;
+        va_start(v, lpszFormat);
+        bool bRet = executeV(lpszFormat, v);
+        va_end(v);
+        return bRet;
+    }
 
-	bool execute(const wchar_t* lpszFormat, ...)
-	{
-		va_list v;
-		va_start(v, lpszFormat);
-		bool bRet = executeV(lpszFormat, v);
-		va_end(v);
-		return bRet;
-	}
+    bool execute(const wchar_t* lpszFormat, ...)
+    {
+        va_list v;
+        va_start(v, lpszFormat);
+        bool bRet = executeV(lpszFormat, v);
+        va_end(v);
+        return bRet;
+    }
 
-	template< typename TYPE >
-	_Rowset_Ptr queryV(const TYPE* lpszFormat, va_list v)
-	{
-		_String strSql;
-		strSql.formatV(lpszFormat, v);
-		return _query(strSql);
-	}
+    template< typename TYPE >
+    _Rowset_Ptr queryV(const TYPE * lpszFormat, va_list v)
+    {
+        _String strSql;
+        strSql.formatV(lpszFormat, v);
+        return _query(strSql);
+    }
 
-	_Rowset_Ptr query(const char* lpszFormat, ...)
-	{
-		va_list v;
-		va_start(v, lpszFormat);
-		_Rowset_Ptr pRet = queryV(lpszFormat, v);
-		va_end(v);
-		return pRet;
-	}
+    _Rowset_Ptr query(const char* lpszFormat, ...)
+    {
+        va_list v;
+        va_start(v, lpszFormat);
+        _Rowset_Ptr pRet = queryV(lpszFormat, v);
+        va_end(v);
+        return pRet;
+    }
 
-	_Rowset_Ptr query(const wchar_t* lpszFormat, ...)
-	{
-		va_list v;
-		va_start(v, lpszFormat);
-		_Rowset_Ptr pRet = queryV(lpszFormat, v);
-		va_end(v);
-		return pRet;
-	}
+    _Rowset_Ptr query(const wchar_t* lpszFormat, ...)
+    {
+        va_list v;
+        va_start(v, lpszFormat);
+        _Rowset_Ptr pRet = queryV(lpszFormat, v);
+        va_end(v);
+        return pRet;
+    }
 
 private:
-	_SQLite m_SQLite;
-	int m_nLastError;
+    _SQLite m_SQLite;
+    int m_nLastError;
 };
 
 #endif
--- src/xsqlitemp.h
+++ src/xsqlitemp.h
@@ -9,277 +9,287 @@ typedef sqlite3_stmt* _SQLiteStmt;
 template< typename _Elem >
 struct sqliteColumn
 {
-	typedef typename sqliteColumn< _Elem > _Ty;
-	typedef typename basic_string< _Elem > _String;
+    typedef typename sqliteColumn< _Elem > _Ty;
+    typedef typename basic_string< _Elem > _String;
 public:
-	sqliteColumn()
-		: m_SQLiteStmt(0), m_iColumn(0) {}
-	sqliteColumn(_SQLiteStmt SQLiteStmt, int iColumn)
-		: m_SQLiteStmt(SQLiteStmt), m_iColumn(iColumn) {}
+    sqliteColumn()
+        : m_SQLiteStmt(0), m_iColumn(0) {}
+    sqliteColumn(_SQLiteStmt SQLiteStmt, int iColumn)
+        : m_SQLiteStmt(SQLiteStmt), m_iColumn(iColumn) {}
 
 public:
-	_String name() const
-	{
-		_String strRet;
-		strRet.fromutf8(sqlite3_column_name(m_SQLiteStmt, m_iColumn));
-		return strRet;
-	}
+    _String name() const
+    {
+        _String strRet;
+        strRet.fromutf8(sqlite3_column_name(m_SQLiteStmt, m_iColumn));
+        return strRet;
+    }
 
-	int type() const
-	{ return sqlite3_column_type(m_SQLiteStmt, m_iColumn); }
+    int type() const
+    {
+        return sqlite3_column_type(m_SQLiteStmt, m_iColumn);
+    }
 
 public:
-	const _Ty& operator=(const _Ty& Other)
-	{
-		if (this != &Other)
-		{
-			m_SQLiteStmt = Other.m_SQLiteStmt;
-			m_iColumn = Other.m_iColumn;
-		}
-		return *this;
-	}
+    const _Ty& operator=(const _Ty& Other)
+    {
+        if (this != &Other)
+        {
+            m_SQLiteStmt = Other.m_SQLiteStmt;
+            m_iColumn = Other.m_iColumn;
+        }
+        return *this;
+    }
 
 protected:
-	_SQLiteStmt m_SQLiteStmt;
-	int m_iColumn;
+    _SQLiteStmt m_SQLiteStmt;
+    int m_iColumn;
 };
 
 template< typename _Elem >
 struct sqliteElement : public sqliteColumn< _Elem >
 {
-	typedef typename sqliteColumn< _Elem > _Base;
-	typedef typename sqliteElement< _Elem > _Ty;
-	typedef typename _Base::_String _String;
+    typedef typename sqliteColumn< _Elem > _Base;
+    typedef typename sqliteElement< _Elem > _Ty;
+    typedef typename _Base::_String _String;
 public:
-	sqliteElement() {}
-	sqliteElement(_SQLiteStmt SQLiteStmt, int iColumn)
-		: _Base(SQLiteStmt, iColumn) {}
-
-public:
-	int size() const
-	{ return sqlite3_column_bytes(m_SQLiteStmt, m_iColumn); }
-
-	const void* as_blob() const
-	{
-		if (type() != SQLITE_BLOB)
-			throw std::exception("Invalid column type");
-		return sqlite3_column_blob(m_SQLiteStmt, m_iColumn);
-	}
-
-	double as_double() const
-	{
-		if (type() != SQLITE_FLOAT)
-			throw std::exception("Invalid column type");
-		return sqlite3_column_double(m_SQLiteStmt, m_iColumn);
-	}
-
-	int as_int() const
-	{
-		if (type() != SQLITE_INTEGER)
-			throw std::exception("Invalid column type");
-		return sqlite3_column_int(m_SQLiteStmt, m_iColumn);
-	}
-
-	_String as_string() const
-	{
-		if (type() != SQLITE_TEXT)
-			throw std::exception("Invalid column type");
-		_String strRet;
-		strRet.fromutf8((const char*)(sqlite3_column_text(m_SQLiteStmt, m_iColumn)));
-		return strRet;
-	}
+    sqliteElement() {}
+    sqliteElement(_SQLiteStmt SQLiteStmt, int iColumn)
+        : _Base(SQLiteStmt, iColumn) {}
 
 public:
-	const _Ty& operator=(const _Ty& Other)
-	{
-		if (this != &Other)
-			_Base::operator=(Other);
-		return *this;
-	}
+    int size() const
+    {
+        return sqlite3_column_bytes(m_SQLiteStmt, m_iColumn);
+    }
+
+    const void* as_blob() const
+    {
+        if (type() != SQLITE_BLOB)
+            throw std::exception("Invalid column type");
+        return sqlite3_column_blob(m_SQLiteStmt, m_iColumn);
+    }
+
+    double as_double() const
+    {
+        if (type() != SQLITE_FLOAT)
+            throw std::exception("Invalid column type");
+        return sqlite3_column_double(m_SQLiteStmt, m_iColumn);
+    }
+
+    int as_int() const
+    {
+        if (type() != SQLITE_INTEGER)
+            throw std::exception("Invalid column type");
+        return sqlite3_column_int(m_SQLiteStmt, m_iColumn);
+    }
+
+    _String as_string() const
+    {
+        if (type() != SQLITE_TEXT)
+            throw std::exception("Invalid column type");
+        _String strRet;
+        strRet.fromutf8((const char*)(sqlite3_column_text(m_SQLiteStmt, m_iColumn)));
+        return strRet;
+    }
+
+public:
+    const _Ty& operator=(const _Ty & Other)
+    {
+        if (this != &Other)
+            _Base::operator=(Other);
+        return *this;
+    }
 };
 
 template< typename _Elem >
 struct sqliteElementConstIterator
 {
-	typedef typename sqliteElementConstIterator< _Elem > _Ty;
-	typedef typename sqliteElement< _Elem > _Element;
+    typedef typename sqliteElementConstIterator< _Elem > _Ty;
+    typedef typename sqliteElement< _Elem > _Element;
 public:
-	sqliteElementConstIterator()
-		: m_SQLiteStmt(0), m_iColumn(0) {}
-	sqliteElementConstIterator(_SQLiteStmt SQLiteStmt, int iColumn)
-		: m_SQLiteStmt(SQLiteStmt), m_iColumn(iColumn) {}
+    sqliteElementConstIterator()
+        : m_SQLiteStmt(0), m_iColumn(0) {}
+    sqliteElementConstIterator(_SQLiteStmt SQLiteStmt, int iColumn)
+        : m_SQLiteStmt(SQLiteStmt), m_iColumn(iColumn) {}
 
 public:
-	bool operator==(const _Ty& Other) const		{ return ((m_SQLiteStmt == Other.m_SQLiteStmt) && (m_iColumn == Other.m_iColumn)); }
-	bool operator!=(const _Ty& Other) const		{ return !(*this == Other); }
-	const _Ty& operator++(int)					{ m_iColumn++; return *this; }
-	const _Ty& operator++()						{ m_iColumn++; return *this; }
-	const _Ty& operator=(const _Ty& Other)
-	{
-		if (this != &Other)
-		{
-			m_SQLiteStmt = Other.m_SQLiteStmt;
-			m_iColumn = Other.m_iColumn;
-		}
-		return *this;
-	}
-	const _Element* operator->()				{ m_Element = _Element(m_SQLiteStmt, m_iColumn); return &m_Element; }
-	const _Element& operator*()					{ m_Element = _Element(m_SQLiteStmt, m_iColumn); return m_Element; }
+    bool operator==(const _Ty& Other) const { return ((m_SQLiteStmt == Other.m_SQLiteStmt) && (m_iColumn == Other.m_iColumn)); }
+    bool operator!=(const _Ty & Other) const { return !(*this == Other); }
+    const _Ty& operator++(int) { m_iColumn++; return *this; }
+    const _Ty& operator++() { m_iColumn++; return *this; }
+    const _Ty& operator=(const _Ty & Other)
+    {
+        if (this != &Other)
+        {
+            m_SQLiteStmt = Other.m_SQLiteStmt;
+            m_iColumn = Other.m_iColumn;
+        }
+        return *this;
+    }
+    const _Element* operator->() { m_Element = _Element(m_SQLiteStmt, m_iColumn); return &m_Element; }
+    const _Element& operator*() { m_Element = _Element(m_SQLiteStmt, m_iColumn); return m_Element; }
 
 protected:
-	_Element m_Element;
-	_SQLiteStmt m_SQLiteStmt;
-	int m_iColumn;
+    _Element m_Element;
+    _SQLiteStmt m_SQLiteStmt;
+    int m_iColumn;
 };
 
 template< typename _Elem >
 struct sqliteElementIterator : public sqliteElementConstIterator< _Elem >
 {
-	typedef typename sqliteElementIterator< _Elem > _Ty;
-	typedef typename sqliteElementConstIterator< _Elem > _Base;
-	typedef typename _Base::_Element _Element;
+    typedef typename sqliteElementIterator< _Elem > _Ty;
+    typedef typename sqliteElementConstIterator< _Elem > _Base;
+    typedef typename _Base::_Element _Element;
 public:
-	sqliteElementIterator() {}
-	sqliteElementIterator(_SQLiteStmt SQLiteStmt, int iColumn)
-		: _Base(SQLiteStmt, iColumn) {}
+    sqliteElementIterator() {}
+    sqliteElementIterator(_SQLiteStmt SQLiteStmt, int iColumn)
+        : _Base(SQLiteStmt, iColumn) {}
 
 public:
-	const _Ty& operator=(const _Ty& Other)
-	{
-		if (this != &Other)
-			_Base::operator=(Other);
-		return *this;
-	}
-	_Element* operator->()						{ m_Element = _Element(m_SQLiteStmt, m_iColumn); return &m_Element; }
-	_Element& operator*()						{ m_Element = _Element(m_SQLiteStmt, m_iColumn); return m_Element; }
+    const _Ty& operator=(const _Ty& Other)
+    {
+        if (this != &Other)
+            _Base::operator=(Other);
+        return *this;
+    }
+    _Element* operator->() { m_Element = _Element(m_SQLiteStmt, m_iColumn); return &m_Element; }
+    _Element& operator*() { m_Element = _Element(m_SQLiteStmt, m_iColumn); return m_Element; }
 };
 
 template< typename _Elem >
 struct sqliteColumnSet
 {
-	typedef typename sqliteColumnSet< _Elem > _Ty;
-	typedef typename sqliteElementConstIterator< _Elem > const_iterator;
-	typedef typename sqliteElementIterator< _Elem > iterator;
-	typedef typename const_iterator::_Element _Element;
-	typedef typename _Element::_String _String;
+    typedef typename sqliteColumnSet< _Elem > _Ty;
+    typedef typename sqliteElementConstIterator< _Elem > const_iterator;
+    typedef typename sqliteElementIterator< _Elem > iterator;
+    typedef typename const_iterator::_Element _Element;
+    typedef typename _Element::_String _String;
 public:
-	sqliteColumnSet() : m_SQLiteStmt(0), m_iColumn(0) {}
-	sqliteColumnSet(_SQLiteStmt SQLiteStmt) : m_SQLiteStmt(SQLiteStmt), m_iColumn(0) {}
-	~sqliteColumnSet() {}
-
-public:
-	int size() const
-	{
-		return sqlite3_column_count(m_SQLiteStmt);
-	}
-
-	const _Element at(int iColumn) const
-	{
-		if (!(iColumn < size()))
-			throw std::exception("Out of column range");
-		return _Element(m_SQLiteStmt, iColumn);
-	}
+    sqliteColumnSet() : m_SQLiteStmt(0), m_iColumn(0) {}
+    sqliteColumnSet(_SQLiteStmt SQLiteStmt) : m_SQLiteStmt(SQLiteStmt), m_iColumn(0) {}
+    ~sqliteColumnSet() {}
 
 public:
-	const_iterator begin() const { return const_iterator(m_SQLiteStmt, 0); }
-	const_iterator end() const { return const_iterator(m_SQLiteStmt, size()); }
-	const_iterator rbegin() const { return const_iterator(m_SQLiteStmt, size() - 1); }
-	const_iterator rend() const { return const_iterator(m_SQLiteStmt, -1); }
+    int size() const
+    {
+        return sqlite3_column_count(m_SQLiteStmt);
+    }
+
+    const _Element at(int iColumn) const
+    {
+        if (!(iColumn < size()))
+            throw std::exception("Out of column range");
+        return _Element(m_SQLiteStmt, iColumn);
+    }
 
 public:
-	iterator begin() { return iterator(m_SQLiteStmt, 0); }
-	iterator end() { return iterator(m_SQLiteStmt, size()); }
-	iterator rbegin() { return iterator(m_SQLiteStmt, size() - 1); }
-	iterator rend() { return iterator(m_SQLiteStmt, -1); }
+    const_iterator begin() const { return const_iterator(m_SQLiteStmt, 0); }
+    const_iterator end() const { return const_iterator(m_SQLiteStmt, size()); }
+    const_iterator rbegin() const { return const_iterator(m_SQLiteStmt, size() - 1); }
+    const_iterator rend() const { return const_iterator(m_SQLiteStmt, -1); }
 
 public:
-	const _Ty& operator=(const _Ty& Other)
-	{
-		if (this != &Other)
-		{
-			m_SQLiteStmt = Other.m_SQLiteStmt;
-			m_iColumn = Other.m_iColumn;
-		}
-		return *this;
-	}
+    iterator begin() { return iterator(m_SQLiteStmt, 0); }
+    iterator end() { return iterator(m_SQLiteStmt, size()); }
+    iterator rbegin() { return iterator(m_SQLiteStmt, size() - 1); }
+    iterator rend() { return iterator(m_SQLiteStmt, -1); }
 
-	const _Element operator[](int iColumn) const
-	{
-		return at(iColumn);
-	}
+public:
+    const _Ty& operator=(const _Ty & Other)
+    {
+        if (this != &Other)
+        {
+            m_SQLiteStmt = Other.m_SQLiteStmt;
+            m_iColumn = Other.m_iColumn;
+        }
+        return *this;
+    }
 
-	_Ty& operator >> (_Element& e) { e = at(m_iColumn++); return *this; }
-	_Ty& operator >> (int& n) { n = at(m_iColumn++).as_int(); return *this; }
-	_Ty& operator >> (double& d) { d = at(m_iColumn++).as_double(); return *this; }
-	_Ty& operator >> (_String& str) { str = at(m_iColumn++).as_string(); return *this; }
+    const _Element operator[](int iColumn) const
+    {
+        return at(iColumn);
+    }
+
+    _Ty& operator >> (_Element & e) { e = at(m_iColumn++); return *this; }
+    _Ty& operator >> (int& n) { n = at(m_iColumn++).as_int(); return *this; }
+    _Ty& operator >> (double& d) { d = at(m_iColumn++).as_double(); return *this; }
+    _Ty& operator >> (_String & str) { str = at(m_iColumn++).as_string(); return *this; }
 
 private:
-	_SQLiteStmt m_SQLiteStmt;
-	int m_iColumn;
+    _SQLiteStmt m_SQLiteStmt;
+    int m_iColumn;
 };
 
 template< typename _Elem >
 struct sqliteStatement
 {
 public:
-	sqliteStatement(_SQLiteStmt SQLiteStmt) : m_SQLiteStmt(SQLiteStmt) {}
-	virtual ~sqliteStatement() { if (m_SQLiteStmt) sqlite3_finalize(m_SQLiteStmt); }
+    sqliteStatement(_SQLiteStmt SQLiteStmt) : m_SQLiteStmt(SQLiteStmt) {}
+    virtual ~sqliteStatement() { if (m_SQLiteStmt) sqlite3_finalize(m_SQLiteStmt); }
 
 public:
-	int step() { return sqlite3_step(m_SQLiteStmt); }
-	int reset() { sqlite3_reset(m_SQLiteStmt); }
+    int step() { return sqlite3_step(m_SQLiteStmt); }
+    int reset() { sqlite3_reset(m_SQLiteStmt); }
 
 protected:
-	sqliteStatement() {}
-	_SQLiteStmt m_SQLiteStmt;
+    sqliteStatement() {}
+    _SQLiteStmt m_SQLiteStmt;
 };
 
 template< typename _Elem >
 struct sqliteExecutor : protected sqliteStatement< _Elem >
 {
-	typedef typename sqliteStatement< _Elem > _Base;
+    typedef typename sqliteStatement< _Elem > _Base;
 public:
-	sqliteExecutor(_SQLiteStmt SQLiteStmt) : _Base(SQLiteStmt) {}
-	virtual ~sqliteExecutor() {}
+    sqliteExecutor(_SQLiteStmt SQLiteStmt) : _Base(SQLiteStmt) {}
+    virtual ~sqliteExecutor() {}
 
 public:
-	int execute() { return _Base::step(); }
+    int execute() { return _Base::step(); }
 };
 
 template< typename _Elem >
 struct sqliteRowSet : protected sqliteStatement< _Elem >
 {
-	typedef typename sqliteRowSet< _Elem > _Ty;
-	typedef typename sqliteStatement< _Elem > _Base;
-	typedef typename sqliteColumnSet< _Elem > _ColumnSet;
+    typedef typename sqliteRowSet< _Elem > _Ty;
+    typedef typename sqliteStatement< _Elem > _Base;
+    typedef typename sqliteColumnSet< _Elem > _ColumnSet;
 public:
-	sqliteRowSet(_SQLiteStmt SQLiteStmt)
-		: _Base(SQLiteStmt), m_nLastResult(SQLITE_ROW) {}
-	virtual ~sqliteRowSet() {}
-
-public:
-	int get_result() const
-	{ return m_nLastResult; }
+    sqliteRowSet(_SQLiteStmt SQLiteStmt)
+        : _Base(SQLiteStmt), m_nLastResult(SQLITE_ROW) {}
+    virtual ~sqliteRowSet() {}
 
 public:
-	bool is_exist_next() const
-	{ return (m_nLastResult == SQLITE_ROW); }
+    int get_result() const
+    {
+        return m_nLastResult;
+    }
 
-	bool to_next()
-	{
-		if (is_exist_next())
-		{
-			m_nLastResult = _Base::step();
-			return (m_nLastResult == SQLITE_ROW);
-		}
-		return false;
-	}
+public:
+    bool is_exist_next() const
+    {
+        return (m_nLastResult == SQLITE_ROW);
+    }
 
-	_ColumnSet column()
-	{ return _ColumnSet(m_SQLiteStmt); }
+    bool to_next()
+    {
+        if (is_exist_next())
+        {
+            m_nLastResult = _Base::step();
+            return (m_nLastResult == SQLITE_ROW);
+        }
+        return false;
+    }
+
+    _ColumnSet column()
+    {
+        return _ColumnSet(m_SQLiteStmt);
+    }
 
 private:
-	int m_nLastResult;
+    int m_nLastResult;
 };
 
 #endif
